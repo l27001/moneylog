53ec3c78d7eacea113c3404713caaa6b89a46519
Mr_B <l27001@ezdomain.ru>
2021-10-08 21:56:46 +0300

    Ошибка http 413

--- app/http_errors.py
+++ app/http_errors.py
@@ -1,6 +1,15 @@
 from flask import render_template, g
 from app import app
 
+@app.errorhandler(413)
+def request_too_large(e):
+    return render_template('error.html',
+        title = f"Ошибка #{e.code}",
+        user = g.user,
+        name = "Тело запроса слишком велико",
+        description = 'Переданные вами данные/файл превышают лимит сервера. \
+        Попробуйте уменьшить передаваемый объём данных.'), e.code
+
 @app.errorhandler(405)
 def not_found(e):
     return render_template('error.html',
@@ -15,7 +24,8 @@ def not_found(e):
         title = f"Ошибка #{e.code}",
         user = g.user,
         name = "Страница не найдена",
-        description = 'Страница которую вы запросили не может быть найдена. Возможно она была удалена или перемещена.'), e.code
+        description = 'Страница которую вы запросили не может быть найдена. \
+        Возможно она была удалена или перемещена.'), e.code
 
 @app.errorhandler(403)
 def no_access(e):
@@ -23,7 +33,8 @@ def no_access(e):
         title = f"Ошибка #{e.code}",
         user = g.user,
         name = "Доступ запрещён",
-        description = 'У Вас нет прав доступа к этому объекту. Файл недоступен для чтения, или сервер не может его прочитать.'), e.code
+        description = 'У Вас нет прав доступа к этому объекту. \
+        Файл недоступен для чтения, или сервер не может его прочитать.'), e.code
 
 @app.errorhandler(401)
 def auth_required(e):
@@ -47,4 +58,5 @@ def internal_error(e):
         title = f"Ошибка #{e.code}",
         user = g.user,
         name = "Внутренняя ошибка сервера",
-        description = 'Произошла неизвестная ошибка. Попробуйте обновить страницу, или вернитесь сюда позже.'), e.code
+        description = 'Произошла неизвестная ошибка. \
+        Попробуйте обновить страницу, или вернитесь сюда позже.'), e.code
